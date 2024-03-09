4f64e374e733ac1bb972547e3533e6fa781c1292
sumitk@deqode.com <sumitk@deqode.com>
2024-03-10 00:49:55 +0530

    added login and logout for mobile screen

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import "./global.css"
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
-import { Auth0Provider } from '@auth0/auth0-react'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
 
 
