109567f3423ef56b12678816f4aad7b768448d8d
sumitk@deqode.com <sumitk@deqode.com>
2024-03-15 00:53:24 +0530

    removed type issue

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -3,7 +3,7 @@ import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
 
-  const {currentUser,isLoading:isGetLoading}  = useGetMyUser()
+  const {currentUser,isLoading:isGetLoading}  =   useGetMyUser()
   const { updateUser, isLoading:isUpdateLoading } = useUpdateMyUser();
 
   if (isGetLoading) {
