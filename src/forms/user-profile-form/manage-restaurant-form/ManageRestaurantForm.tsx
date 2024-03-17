ef06d159d960d715f5682198b58a6105f45a1cce
sumitk@deqode.com <sumitk@deqode.com>
2024-03-18 01:11:30 +0530

    worked on manage restaurant page

---
+++ src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,76 @@
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import DetailsSection from "./DetailsSection";
+import { Separator } from "@radix-ui/react-separator";
+import CuisinesSection from "./CuisinesSection";
+import MenSection from "./MenSection";
+
+const formSchema = z.object({
+  restaurantName: z.string({
+    required_error: "resturant name is required",
+  }),
+  city: z.string({
+    required_error: "city name is required",
+  }),
+  country: z.string({
+    required_error: "country name is required",
+  }),
+  deliveryPrice: z.coerce.number({
+    required_error: "delivery price is required",
+    invalid_type_error: "must be  a valid number",
+  }),
+  estimatedDeliveryTime: z.coerce.number({
+    required_error: "estimated delivery time is required",
+    invalid_type_error: "must be  a valid number",
+  }),
+  cuisines: z.array(z.string()).nonempty({
+    message: "Please select at least one item",
+  }),
+  menuItems: z.array(
+    z.object({
+      name: z.string().min(1, "name is required"),
+      price: z.coerce.number().min(1, "price is requirede"),
+    })
+  ),
+  imageFile: z.instanceof(File, { message: "image is required" }),
+});
+
+type restaurantFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  onSave: (restaurantFormData: FormData) => void;
+  isLoading: boolean;
+};
+
+const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
+  const form = useForm<restaurantFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
+
+  const onSubmit = (formDataJson: restaurantFormData) => {
+    // TODO - conver fordDataJSOn to a new  FormData Object
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+        <Separator />
+        <CuisinesSection />
+        <Separator />
+        <MenSection />
+      </form>
+    </Form>
+  );
+};
+
+export default ManageRestaurantForm;
---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,6 @@
+import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
+import React from "react";
+
+export default function () {
+  return <ManageRestaurantForm />;
+}
